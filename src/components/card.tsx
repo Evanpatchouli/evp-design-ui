698c65b425cc3243e1dfa98aac66eac5657e0031
Evanpatchouli <3125019716@qq.com>
2023-08-31 18:46:39 +0800

    docs: evp-popover

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,9 +1,37 @@
-export default function Card(props: { children: React.ReactNode }) {
-  return(
-    <div className="evp evp-base-shadow" style={{
-      padding: 20
-    }}>{
-      props.children}
+import "./card.css";
+
+export default function Card(props: {
+  children: React.ReactNode;
+  style?: React.CSSProperties;
+  contentStyle?: React.CSSProperties;
+  toolbarStyle?: React.CSSProperties;
+  toolBar?: React.ReactNode;
+  class?: string;
+  contentClass?: string;
+  toolBarClass?: string;
+}) {
+  return (
+    <div
+      className={`evp evp-base-shadow card ${props.class ?? ""}`.trim()}
+      style={{
+        ...props.style,
+      }}
+    >
+      <div
+        className={`card-content ${props.contentClass}`.trim()}
+        style={{ ...props.contentStyle }}
+      >
+        {props.children}
+      </div>
+      <div
+        className={`card-toolbar ${props.toolBarClass}`.trim()}
+        style={{
+          borderTop: props.toolBar? '1px solid rgba(0,0,0,0.05)' : void 0,
+          ...props.toolbarStyle
+        }}
+      >
+        {props.toolBar}
+      </div>
     </div>
-  )
+  );
 }
--- src/components/code.tsx
+++ src/components/code.tsx
@@ -1,7 +1,7 @@
 import { Prism as Highlight } from 'react-syntax-highlighter';
 import { vscDarkPlus }  from 'react-syntax-highlighter/dist/esm/styles/prism';// 代码高亮主题风格
 
-type LangOptions = 'javascript' | 'typescript' | 'java' | 'python' | 'c' | 'cpp' | 'csharp' | 'go' | 'ruby' | 'rust' | 'swift' | 'kotlin' | 'php' | 'html' | 'css' | 'scss' | 'less' | 'json' | 'xml' | 'yml' | 'yaml' | 'bash' | 'sh' | 'shell' | 'cmd' | 'bat' | 'console' | 'sql' | 'markdown' | 'text';
+type LangOptions = 'javascript' | 'typescript' | 'jsx' | 'tsx' | 'java' | 'python' | 'c' | 'cpp' | 'csharp' | 'go' | 'ruby' | 'rust' | 'swift' | 'kotlin' | 'php' | 'html' | 'css' | 'scss' | 'less' | 'json' | 'xml' | 'yml' | 'yaml' | 'bash' | 'sh' | 'shell' | 'cmd' | 'bat' | 'console' | 'sql' | 'markdown' | 'text';
 
 export default function Code(props: { children?: string, lang?: Hintable<LangOptions> }){
   return(
